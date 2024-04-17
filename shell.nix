be91aafb582d50ccfafde67be599a6d740f2c431
greysoh <me@greysoh.dev>
2024-04-17 17:55:40 -0400

    feature: Adds NixOS support.

--- shell.nix
+++ shell.nix
@@ -2,5 +2,10 @@ let
   pkgs = import (fetchTarball ("channel:nixpkgs-unstable")) { };
 in
 pkgs.mkShell {
-  buildInputs = [ pkgs.nodejs pkgs.sqlite ];
+  buildInputs = [ pkgs.nodejs pkgs.sqlite pkgs.openssl ];
+  shellHook = ''
+    export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
+    export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
+    export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
+  '';
 }
