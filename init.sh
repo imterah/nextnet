1ae93d71f670cfa3873bc72bfef56fd352d05c0c
greysoh <me@greysoh.dev>
2024-04-21 16:09:20 -0400

    chore: Migrates development to use PostgreSQL instead.

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+if [ ! -d ".tmp" ]; then
+  echo "Hello and welcome to the NextNet project! Please wait while I initialize things for you..."
+  cp dev.env .env
+  mkdir .tmp
+fi
+
+lsof -i:5432 | grep postgres
+IS_PG_RUNNING=$?
+
+if [ ! -f ".tmp/ispginit" ]; then
+  if [[ "$IS_PG_RUNNING" == 0 ]]; then
+    kill -9 $(lsof -t -i:5432)
+  fi
+
+  echo " - Database not initialized! Initializing database..."
+  mkdir .tmp/pglock
+
+  initdb -D .tmp/db
+  pg_ctl -D .tmp/db -l .tmp/logfile -o "--unix_socket_directories='$PWD/.tmp/pglock/'" start
+  createdb -h localhost -p 5432 nextnet 
+
+  psql -h localhost -p 5432 nextnet -c "CREATE ROLE nextnet WITH LOGIN SUPERUSER PASSWORD 'nextnet';"
+      
+  npm install --save-dev
+  npx prisma migrate dev
+      
+  touch .tmp/ispginit
+elif [[ "$IS_PG_RUNNING" == 1 ]]; then
+  pg_ctl -D .tmp/db -l .tmp/logfile -o "--unix_socket_directories='$PWD/.tmp/pglock/'" start
+fi
+
+source .env # Make sure we actually load correctly
--- shell.nix
+++ shell.nix
@@ -2,10 +2,12 @@ let
   pkgs = import (fetchTarball ("channel:nixpkgs-unstable")) { };
 in
 pkgs.mkShell {
-  buildInputs = [ pkgs.nodejs pkgs.sqlite pkgs.openssl ];
+  buildInputs = [ pkgs.nodejs pkgs.openssl pkgs.postgresql pkgs.lsof ];
   shellHook = ''
     export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
     export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
     export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
+
+    bash init.sh
   '';
 }
