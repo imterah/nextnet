0e3335c4c2a84d453fc2fa7ee9fcd631a6d36d2a
Greyson <me@greysoh.dev>
2024-05-11 16:57:40 -0400

    chore: Remove remaining Nix shell stuff.

--- shell.nix
+++ shell.nix
@@ -2,11 +2,6 @@
   pkgs ? import <nixpkgs> { },
 }: pkgs.mkShell {
   buildInputs = with pkgs; [ 
-    # gui/
-    cargo
-    rustc
-    rustfmt
-    
     # api/
     nodejs
     openssl
@@ -18,15 +13,6 @@
     export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
     export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
     export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
-
-    # egui patches!
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.wayland}/lib
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libxkbcommon}/lib
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.xorg.libX11}/lib
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.xorg.libXcursor}/lib
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.xorg.libXrandr}/lib
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.xorg.libXi}/lib
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libGL}/lib
 
     if [ ! -d ".tmp" ]; then
       echo "Hello and welcome to the NextNet project!"
