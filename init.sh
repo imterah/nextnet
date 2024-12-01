0965c565470167b80eb0e31d579100cd82e21d2f
greysoh <me@greysoh.dev>
2024-12-01 13:06:28 -0500

    chore: Bumps dependencies, rewrites development environment system.

--- init.sh
+++ init.sh
@@ -1,5 +1,35 @@
-pushd api > /dev/null 2> /dev/null
-source init.sh 
+#!/usr/bin/env bash
+owned_docker=1
 
-git config --local include.path .gitconfig
-popd > /dev/null 2> /dev/null
+# Test if Postgres is up
+lsof -i:5432 2> /dev/null > /dev/null
+
+if [ $? -ne 0 ]; then
+  owned_docker=0
+  docker compose -f dev-docker-compose.yml up -d
+fi
+
+if [ ! -f "api/.env" ]; then
+  cp api/dev.env api/.env
+fi
+
+if [ ! -d "api/node_modules" ]; then
+  pushd api > /dev/null
+  npm install --save-dev
+  npx prisma migrate dev
+  popd > /dev/null
+fi
+
+source api/.env
+
+on_exit() {
+  cd $(git rev-parse --show-toplevel)
+
+  if [ $owned_docker -ne 0 ]; then
+    return
+  fi
+
+  docker compose -f dev-docker-compose.yml down
+}
+
+trap "on_exit" exit
--- shell.nix
+++ shell.nix
@@ -1,24 +1,20 @@
 {
   pkgs ? import <nixpkgs> { },
 }: pkgs.mkShell {
-  buildInputs = with pkgs; [ 
+  buildInputs = with pkgs; [
     # api/
     nodejs
     openssl
-    postgresql
     lsof
-  ];  
+    go
+    gopls
+  ];
 
   shellHook = ''
     export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
     export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
     export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
 
-    if [ ! -d ".tmp" ]; then
-      echo "Hello and welcome to the NextNet project!"
-      mkdir .tmp
-    fi
-    
     source init.sh
   '';
 }
