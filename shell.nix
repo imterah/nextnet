be92c5a569d08c68b920e3591339e8f7b2262525
imterah <me@terah.dev>
2024-12-28 12:11:08 -0500

    chore: Slims up shell.nix.

--- shell.nix
+++ shell.nix
@@ -4,17 +4,11 @@
   buildInputs = with pkgs; [
     # api/
     nodejs
-    openssl
-    lsof
     go
     gopls
   ];
 
   shellHook = ''
-    export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
-    export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
-    export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
-
     source init.sh
   '';
 }
