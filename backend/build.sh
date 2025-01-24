a35602a6f24ad6e2c03fcc2e1dc9f9a3446a0f06
imterah <me@terah.dev>
2025-01-24 13:26:25 -0500

    chore: Initialize sshappbackend.

--- backend/build.sh
+++ backend/build.sh
@@ -1,20 +1,41 @@
 #!/usr/bin/env bash
-pushd sshbackend
-GOOS=linux go build .
-strip sshbackend
-popd
+pushd sshbackend > /dev/null
+echo "building sshbackend"
+go build -ldflags="-s -w" -trimpath .
+popd > /dev/null
 
-pushd dummybackend
-GOOS=linux go build .
-strip dummybackend
-popd
+pushd dummybackend > /dev/null
+echo "building dummybackend"
+go build -ldflags="-s -w" -trimpath .
+popd > /dev/null
 
-pushd externalbackendlauncher
-go build .
-strip externalbackendlauncher
-popd
+pushd externalbackendlauncher > /dev/null
+echo "building externalbackendlauncher"
+go build -ldflags="-s -w" -trimpath .
+popd > /dev/null
 
-pushd api
-GOOS=linux go build .
-strip api
-popd
+pushd sshappbackend/remote-code > /dev/null
+echo "building sshappbackend/remote-code"
+if [ ! -d bin ]; then
+    mkdir bin
+fi
+
+echo " - building for arm64"
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -trimpath -o bin/rt-arm64 .
+echo " - building for arm"
+GOOS=linux GOARCH=arm go build -ldflags="-s -w" -trimpath -o bin/rt-arm .
+echo " - building for amd64"
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -trimpath -o bin/rt-amd64 .
+echo " - building for i386"
+GOOS=linux GOARCH=386 go build -ldflags="-s -w" -trimpath -o bin/rt-386 .
+popd > /dev/null
+
+pushd sshappbackend/local-code > /dev/null
+echo "building sshappbackend/local-code"
+go build -ldflags="-s -w" -trimpath -o sshappbackend .
+popd > /dev/null
+
+pushd api > /dev/null
+echo "building api"
+go build -ldflags="-s -w" -trimpath .
+popd > /dev/null
